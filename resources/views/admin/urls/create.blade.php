955b652aa04bfb0db41d48f88150a37b4a269ba2
Widya <widyaasaputri30@gmail.com>
2025-09-01 09:20:12 +0700

    update

--- resources/views/admin/urls/create.blade.php
+++ resources/views/admin/urls/create.blade.php
@@ -51,6 +51,15 @@
 
                     <!-- Bidang - (Read Only)-->
                     <div class="mb-4">
+                        <label for="bidang_id" class="block text-sm font-medium text-gray-700">Seksi</label>
+                            <div class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm px-3 py-2 bg-gray-100 text-gray-600">
+                                {{ auth()->user()->name ?? 'N/A' }}
+                            </div>
+                        <!-- Input hidden ini yang mengirimkan ID Bidang ke controller -->
+                        <input type="hidden" name="users_id" value="{{ auth()->user()->id }}">
+                    </div>
+                    
+                    <div class="mb-4">
                         <label for="bidang_id" class="block text-sm font-medium text-gray-700">Bidang</label>
                             <div class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm px-3 py-2 bg-gray-100 text-gray-600">
                                 {{ auth()->user()->bidang->nama_bidang ?? 'N/A' }}
@@ -58,6 +67,7 @@
                         <!-- Input hidden ini yang mengirimkan ID Bidang ke controller -->
                         <input type="hidden" name="bidang_id" value="{{ auth()->user()->bidang_id }}">
                     </div>
+                    
 
                     <!-- Seksi - Otomatis terisi -->
                     <!-- Input ini yang mengirimkan ID Seksi ke controller -->
